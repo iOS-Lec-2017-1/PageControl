2ae1c1ae3c6d8478deb7144cd36224d67d8da6f9
김종현 <jhkim@dit.ac.kr>
2017-01-25 00:16:35 +0900

    initial commit

--- PageControl/ViewController.swift
+++ PageControl/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var images = ["01.png", "02.png", "03.png", "04.png", "05.png", "06.png"]
 
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.green
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        imgView.image = UIImage(named: images[0])
+
     }
 
+    @IBAction func pageChangeed(_ sender: UIPageControl) {
+        
+        imgView.image = UIImage(named: images[pageControl.currentPage])
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
